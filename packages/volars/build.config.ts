5690433ad7b099c70a7db16baf25ad5a7b9eebe8
Arexon <arexonreal@gmail.com>
2022-06-07 01:19:12 +0200

    fix(config): issue in build mode

--- packages/volars/build.config.ts
+++ packages/volars/build.config.ts
@@ -3,6 +3,12 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
 	clean: true,
 	declaration: true,
-	entries: ['src/index', 'src/cli'],
-	dependencies: ['c12', 'vite', 'vite-node', 'cac', 'pathe', 'fast-glob', 'consola', 'chalk']
+	entries: ['src/index', 'src/cli', 'src/config'],
+	dependencies: ['c12', 'vite', 'vite-node', 'cac', 'pathe', 'fast-glob', 'consola', 'chalk'],
+	rollup: {
+		inlineDependencies: true,
+		esbuild: {
+			target: 'esnext'
+		}
+	}
 })
