8694b0214f7577c84ae92da3a18b8730827ec0ed
Arexon <arexonreal@gmail.com>
2022-06-08 00:10:14 +0200

    feat(compiler): implement watch mode

--- packages/volars/build.config.ts
+++ packages/volars/build.config.ts
@@ -4,7 +4,7 @@ export default defineBuildConfig({
 	clean: true,
 	declaration: true,
 	entries: ['src/index', 'src/cli', 'src/config'],
-	dependencies: ['c12', 'mri', 'pathe', 'globby', 'consola'],
+	dependencies: ['c12', 'mri', 'pathe', 'globby', 'consola', 'chokidar', 'perfect-debounce'],
 	rollup: {
 		inlineDependencies: true,
 		esbuild: {
--- packages/volars/src/compiler/build.ts
+++ packages/volars/src/compiler/build.ts
@@ -1,7 +1,9 @@
 import jiti from 'jiti'
 import { resolve } from 'path'
 import { globby } from 'globby'
-import { Packs, Volars } from '../types/volars'
+import { watch } from 'chokidar'
+import { debounce } from 'perfect-debounce'
+import type { Packs, Volars } from '../types/volars'
 
 export async function build(volars: Volars): Promise<void> {
 	return volars.dev ? _watch(volars) : _build(volars)
@@ -13,25 +15,54 @@ async function _build(volars: Volars): Promise<void> {
 	volars.logger.start('Building the project...')
 
 	const start = Date.now()
-	const results = await Promise.allSettled(
-		files.map(async (file) => {
-			await jiti('', { interopDefault: true })(file)
-		})
-	)
+	const results = await Promise.allSettled(files.map(async (file) => await executeFile(file)))
 
 	volars.logger.success(`Compiled ${results.length} files in ${Date.now() - start} ms`)
 }
 
 async function _watch(volars: Volars): Promise<void> {
+	interface WatchedFilesQueue {
+		updated?: string[]
+		deleted?: string[]
+	}
+
 	volars.logger.start('Watching the project...')
+
+	let watchedFilesQueue: WatchedFilesQueue = { updated: [], deleted: [] }
+
+	const reload = debounce(async () => {
+		await Promise.all(
+			watchedFilesQueue.updated!.map(async (file) => {
+				await executeFile(file)
+			})
+		)
+
+		volars.logger.success(watchedFilesQueue)
+
+		watchedFilesQueue = { updated: [], deleted: [] }
+	}, 0)
+
+	watch(volars.config.packs.behaviorPack, {
+		ignoreInitial: true
+	}).on('all', async (event, path) => {
+		if (event === 'change' || event === 'add') {
+			watchedFilesQueue.updated?.push(getAboslutePath(path))
+		} else if (event === 'unlink') {
+			watchedFilesQueue.deleted?.push(getAboslutePath(path))
+		}
+
+		await reload()
+	})
 }
 
 function getAboslutePath(path: string): string {
-	return resolve(process.cwd(), path)
+	return resolve(process.cwd(), path).replace(/\\/g, '/')
 }
 
 async function getAllFiles(packs: Packs): Promise<string[]> {
-	return await globby([
-		getAboslutePath(`${packs.behaviorPack}/blocks/*.block.ts`).replace(/\\/g, '/')
-	])
+	return await globby([getAboslutePath(`${packs.behaviorPack}/blocks/*.block.ts`)])
+}
+
+async function executeFile(file: string): Promise<void> {
+	await jiti('', { interopDefault: true })(file)
 }
