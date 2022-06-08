0ea6124859c181b1b3c0e163034e187efdff2581
Arexon <arexonreal@gmail.com>
2022-06-09 00:10:02 +0200

    fix(core): move block objects into `minecraft:block`

--- packages/volars/src/core/block.ts
+++ packages/volars/src/core/block.ts
@@ -4,11 +4,12 @@ import type { BlockTemplate } from './types'
 export const defineBlock = async (fn: (template: BlockTemplate) => void): Promise<Object> => {
 	let src: Object = {}
 	const config = await loadVolarsConfig()
+	const block = 'minecraft:block'
 
 	const template: BlockTemplate = {
 		namespace: config.namespace,
 		formatVersion: (format_version) => (src = { format_version }),
-		description: (description) => (src = { ...src, description })
+		description: (description) => (src = { ...src, [block]: { description } })
 	}
 	fn(template)
 
