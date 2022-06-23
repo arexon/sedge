ae216e405e10d36c78ffbe85d33684708f189c5c
Arexon <arexonreal@gmail.com>
2022-06-23 20:14:10 +0200

    feat(config): error handling

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,6 +1,9 @@
 import jiti from 'jiti'
 import { resolve } from 'pathe'
+import consola from 'consola'
 import { Config } from './types'
+
+const logger = consola.withTag('volars config')
 
 export async function loadConfig(): Promise<Config> {
 	const path = resolve(process.cwd(), 'config.json')
@@ -8,7 +11,24 @@ export async function loadConfig(): Promise<Config> {
 		interopDefault: true
 	})(path)
 
+	validateConfig(config)
+
 	return config
+}
+
+function validateConfig(config: Config): void {
+	const requiredProperties = ['name', 'namespace', 'packs']
+	let missingProperties: string[] = []
+
+	for (const property of requiredProperties) {
+		if (config.hasOwnProperty(property)) continue
+		missingProperties.push(property)
+	}
+
+	if (missingProperties.length > 0) {
+		logger.error('Missing required properties:', missingProperties)
+		process.exit(1)
+	}
 }
 
 export * from './types'
