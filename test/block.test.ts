4294e914e9454b4488bc101bf2eb15d991057502
Arexon <arexonreal@gmail.com>
2022-07-02 13:13:47 +0200

    test: import fixtures from playground

--- test/block.test.ts
+++ test/block.test.ts
@@ -1,119 +1,46 @@
 import { describe, expect, it } from 'vitest'
-import { defineBlock } from '../packages/volars'
-import { blockComponent } from './common'
+import block from '#playground/BP/blocks/radio.block'
 
 describe('define block', () => {
-	it.concurrent('return a block', () => {
-		const block = defineBlock(
-			'1.19.10',
-			({ namespace, description, components, permutations, events }) => {
-				const isBreathable = `${namespace}:is_breathable`
-				const makeBreathable = `${namespace}:make_breathable`
-
-				description({
-					identifier: `${namespace}:foo`,
-					properties: { [isBreathable]: [false, true] }
-				})
-
-				permutations([
-					{
-						condition: `q.block_property('${isBreathable}') == true`,
-						components: { breathability: 'air' }
-					}
-				])
-
-				components({
-					on_interact: {
-						condition: 'q.is_sneaking',
-						event: makeBreathable
-					}
-				})
-
-				events({
-					[makeBreathable]: {
-						set_block_property: { [isBreathable]: true }
-					}
-				})
-			}
-		)
-
+	it('returns a valid block object', () => {
 		expect(block).toStrictEqual({
 			format_version: '1.19.10',
 			'minecraft:block': {
 				description: {
-					identifier: 'test:foo',
-					properties: { 'test:is_breathable': [false, true] }
+					properties: {
+						'test:is_on': [false, true]
+					},
+					identifier: 'test:radio'
 				},
 				components: {
 					'minecraft:on_interact': {
 						condition: 'q.is_sneaking',
-						event: 'test:make_breathable'
-					}
+						event: 'test:toggle'
+					},
+					'minecraft:display_name': 'test:radio',
+					'minecraft:loot': 'loot_tables/block/radio.loot.json'
 				},
 				permutations: [
 					{
-						condition: `q.block_property('test:is_breathable') == true`,
-						components: { 'minecraft:breathability': 'air' }
+						condition: "q.block_property('test:is_on') == false",
+						components: {
+							'minecraft:geometry': 'geometry.radio.off'
+						}
+					},
+					{
+						condition: "q.block_property('test:is_on') == true",
+						components: {
+							'minecraft:geometry': 'geometry.radio.on'
+						}
 					}
 				],
 				events: {
-					'test:make_breathable': {
-						set_block_property: { 'test:is_breathable': true }
+					'test:toggle': {
+						set_block_property: {
+							'test:is_on': "!q.block_property('test:is_on')"
+						}
 					}
 				}
-			}
-		})
-	})
-
-	it.concurrent('return a legacy block', () => {
-		const block = defineBlock(
-			'1.16.0',
-			({ namespace, components, description }) => {
-				const burnOdds = 30
-
-				description({
-					identifier: `${namespace}:foo`
-				})
-
-				components({
-					flammable: { burn_odds: burnOdds, flame_odds: burnOdds }
-				})
-			}
-		)
-
-		expect(block).toStrictEqual({
-			format_version: '1.16.0',
-			'minecraft:block': {
-				description: { identifier: 'test:foo' },
-				components: {
-					'minecraft:flammable': { burn_odds: 30, flame_odds: 30 }
-				}
-			}
-		})
-	})
-
-	it.concurrent('return a block with a custom component', () => {
-		const block = defineBlock('1.19.10', () => {}, [
-			blockComponent({ amount: 3 })
-		])
-
-		expect(block).toStrictEqual({
-			format_version: '1.19.10',
-			'minecraft:block': {
-				permutations: [
-					{
-						condition: `q.block_property('test:foo') == 1`,
-						components: { 'minecraft:geometry': 'geometry.bar1' }
-					},
-					{
-						condition: `q.block_property('test:foo') == 2`,
-						components: { 'minecraft:geometry': 'geometry.bar2' }
-					},
-					{
-						condition: `q.block_property('test:foo') == 3`,
-						components: { 'minecraft:geometry': 'geometry.bar3' }
-					}
-				]
 			}
 		})
 	})
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+	resolve: {
+		alias: {
+			volars: '/packages/volars/src',
+			'#playground/BP': '../playground/packs/BP'
+		}
+	},
 	define: {
 		config: {
 			namespace: 'test'
