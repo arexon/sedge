a6854e711e79f1d9d1ec53e7a749e9c1758738b4
Arexon <arexonreal@gmail.com>
2022-07-06 13:15:20 +0200

    test: use snapshots

--- test/block.test.ts
+++ test/block.test.ts
@@ -3,44 +3,6 @@ import block from '#playground/BP/blocks/radio.block'
 
 describe('define block', () => {
 	it('returns a valid block object', () => {
-		expect(block).toStrictEqual({
-			format_version: '1.19.10',
-			'minecraft:block': {
-				description: {
-					properties: {
-						'test:is_on': [false, true]
-					},
-					identifier: 'test:radio'
-				},
-				components: {
-					'minecraft:on_interact': {
-						condition: 'q.is_sneaking',
-						event: 'test:toggle'
-					},
-					'minecraft:display_name': 'test:radio'
-				},
-				permutations: [
-					{
-						condition: "q.block_property('test:is_on') == false",
-						components: {
-							'minecraft:geometry': 'geometry.radio.off'
-						}
-					},
-					{
-						condition: "q.block_property('test:is_on') == true",
-						components: {
-							'minecraft:geometry': 'geometry.radio.on'
-						}
-					}
-				],
-				events: {
-					'test:toggle': {
-						set_block_property: {
-							'test:is_on': "!q.block_property('test:is_on')"
-						}
-					}
-				}
-			}
-		})
+		expect(block).toMatchSnapshot()
 	})
 })
