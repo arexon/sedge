7144cf2b668362e97e12e591e47d2fbd0fd79090
Arexon <arexonreal@gmail.com>
2022-07-06 14:58:47 +0200

    fix(runtime): module extension not set as `.json` in watch mode

--- packages/volars/src/runtime/build.ts
+++ packages/volars/src/runtime/build.ts
@@ -87,9 +87,11 @@ export async function watch(): Promise<void> {
 			// Guard for whether the file is a module or an asset
 			if (extname(path) === '.ts') {
 				const content = await loadModule(path, true)
-				fse.outputJSONSync(resolve(global.target.path, path), content, {
-					spaces: '\t'
-				})
+				fse.outputJSONSync(
+					resolve(global.target.path, changeExt(path, '.json')),
+					content,
+					{ spaces: '\t' }
+				)
 			} else {
 				fse.copySync(path, resolve(global.target.path, path))
 			}
