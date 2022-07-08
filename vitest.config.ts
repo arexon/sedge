7b3b57460d44a49efc1e14b47ffcd705f1c6ac1c
Arexon <arexonreal@gmail.com>
2022-07-08 17:30:06 +0200

    test: update tests config and scripts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
 	resolve: {
 		alias: {
-			'volars/schema': '/packages/volars/src',
 			'#playground/BP': '../playground/packs/BP',
 			'#components': '/playground/packs/BP/components'
 		}
