83f4c35c4a67fef1ebeacb5bf949a689dc04aeb5
Arexon <arexonreal@gmail.com>
2022-07-08 23:34:03 +0200

    test: remove unneeded vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
 	resolve: {
 		alias: {
-			'#playground/BP': '../playground/packs/BP',
-			'#components': '/playground/packs/BP/components'
+			'#playground/BP': '../playground/packs/BP'
 		}
 	},
 	define: {
