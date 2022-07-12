6b7132080d32e24442512d17ca15db43af948ac2
Arexon <arexonreal@gmail.com>
2022-07-13 00:13:51 +0200

    test: fix to work in dev

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
 	resolve: {
 		alias: {
+			'atropa/server': '/packages/atropa/src/core/server',
 			'#playground/BP': '../playground/packs/BP'
 		}
 	},
