1e6fbc0839067fc7962d36c99817019315e9798c
Arexon <arexonreal@gmail.com>
2022-07-20 12:24:54 +0200

    refactor(core): remove unused utility functions

--- packages/atropa/src/core/utils.ts
+++ packages/atropa/src/core/utils.ts
@@ -1,14 +1,5 @@
 import { objectMap } from '@antfu/utils'
 
-export function removeEmptyProperties<T extends Record<string, any>>(
-	object: T
-): T {
-	for (const key in object) {
-		if (objectIsEmpty(object[key])) delete object[key]
-	}
-	return object
-}
-
 export function ensureNamespaces<K extends string, V>(
 	object: Record<K, V>,
 	namespace: string
@@ -21,9 +12,4 @@ export function ensureNamespaces<K extends string, V>(
 function ensurePrefix<T extends string>(prefix: string, string: T): T {
 	if (!string.startsWith(prefix)) return `${prefix}:${string}` as T
 	return string
-}
-
-function objectIsEmpty<T extends Record<string, any>>(object: T): boolean {
-	for (const _ in object) return false
-	return true
 }
