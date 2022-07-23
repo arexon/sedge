5ff4738cfef3d41c4263678614b422a302612b11
Arexon <arexonreal@gmail.com>
2022-07-23 23:38:32 +0200

    fix(core): export `useHook`

--- packages/atropa/src/core/index.ts
+++ packages/atropa/src/core/index.ts
@@ -1,6 +1,9 @@
 export { defineCollection } from './generators/collection'
 export { defineComponent } from './generators/component'
+
 export { defineBlock } from './generators/block'
 export { defineItem } from './generators/item'
 export { defineLootTable } from './generators/loot-table'
 export { defineRecipe } from './generators/recipe'
+
+export { useHook } from './hooks'
