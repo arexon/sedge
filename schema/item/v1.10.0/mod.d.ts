0d00660706144a5a78f985f6ccd1f371cecee777
Arexon <arexonreal@gmail.com>
2022-08-19 22:40:15 +0200

    refactor: move modules to top-level

---
+++ schema/item/v1.10.0/mod.d.ts
@@ -0,0 +1,13 @@
+import * as Components_1_10_0 from './components.d.ts';
+
+interface ItemComponents_1_10_0
+	extends
+		Components_1_10_0.Foil,
+		Components_1_10_0.Food,
+		Components_1_10_0.HandEquipped,
+		Components_1_10_0.MaxDamage,
+		Components_1_10_0.MaxStackSize,
+		Components_1_10_0.StackedByData,
+		Components_1_10_0.UseDuration {}
+
+export { ItemComponents_1_10_0 };
