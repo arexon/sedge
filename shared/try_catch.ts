cdd624f3d2245096eadd42606eaa8d03f0d34cdc
Arexon <arexonreal@gmail.com>
2022-08-20 01:23:02 +0200

    feat(shared): show caller path in `tryCatch`

--- shared/try_catch.ts
+++ shared/try_catch.ts
@@ -1,10 +1,23 @@
+import { fromFileUrl } from 'path';
+import { logger } from './logger.ts';
+
 export function tryCatch<T>(
 	fn: () => T,
-	message: string,
 ): T {
 	try {
 		return fn();
 	} catch (error) {
-		throw new Error(`${message}: ${error}`);
+		logger.error(`${error} at (${getCallerFilePath()})`);
+		Deno.exit(1);
 	}
+}
+
+function getCallerFilePath(): string {
+	const { stack } = new Error();
+
+	if (!stack) return '';
+
+	const stackLines = stack.split('\n');
+	const path = stackLines[stackLines.length - 1].trim().split(' ')[1];
+	return fromFileUrl(path);
 }
