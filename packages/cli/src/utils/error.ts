174bfac1d3c8c7353fc2e11674e160149596f40a
Arexon <arexonreal@gmail.com>
2022-08-20 23:13:40 +0000

    chore: remove unnecessary files

--- packages/cli/src/utils/error.ts
+++
@@ -1,7 +0,0 @@
-export function tryCatch<T>(fn: () => T, message: string): T {
-	try {
-		return fn()
-	} catch (error) {
-		throw new Error(`${message}: ${error}`)
-	}
-}
--- packages/sedge/src/compiler/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './config'
-export { definePlugin } from './plugin'
-export * from './sedge'
