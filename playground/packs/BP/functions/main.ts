c5e67488c40fa18a3ca854a097eeec03f08673f5
Arexon <arexonreal@gmail.com>
2022-08-20 23:13:40 +0000

    chore: remove unnecessary files

--- playground/packs/BP/functions/main.ts
+++
@@ -1,7 +0,0 @@
-import { defineMCFunction } from 'sedge/core'
-
-export default defineMCFunction(({ add }) => {
-	const animals = ['sheep', 'pig', 'chicken']
-
-	animals.forEach((animal) => add(`say ${animal}`))
-})
