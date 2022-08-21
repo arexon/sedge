e0dc348ac7ca3ef11033ea5229edbf437b75ce5f
Arexon <arexonreal@gmail.com>
2022-08-21 23:23:43 +0200

    refactor(core): remove block element and organize types

--- core/elements/common.ts
+++ core/elements/common.ts
@@ -1,8 +1,3 @@
-export interface Result<Data extends Record<string, any> | string> {
-	type: 'gameElement';
-	data: Data;
-}
-
 export interface Namespace {
 	/** The project namespace defined in `config.json`. */
 	namespace: string;
