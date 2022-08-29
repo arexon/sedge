71355d0a0ec2d64274e14f906fe2b9e0d950e967
arexon <arexonreal@gmail.com>
2022-08-30 00:22:15 +0200

    fix(shared): time not updating during a session

--- shared/logger.ts
+++ shared/logger.ts
@@ -1,5 +1,5 @@
 import { format } from 'datetime';
-import { bgRed, bgYellow, bold, cyan, dim, green } from 'fmt/colors.ts';
+import { bgRed, bgYellow, bold, cyan, dim, green, red } from 'fmt/colors.ts';
 
 interface Logger {
 	clear(): void;
@@ -14,42 +14,27 @@ interface Logger {
 export const logger = createLogger();
 
 function createLogger(): Logger {
-	const time = dim(format(new Date(), 'hh:mm:ss'));
-
 	return {
 		clear: () => console.clear(),
-		log: (...data) => console.log(formatData({ data, time })),
-		info: (...data) => {
-			return console.log(
-				formatData({ level: cyan(bold('i')), data, time }),
-			);
-		},
-		success: (...data) => {
-			return console.log(
-				formatData({ level: green(bold('✔')), data, time }),
-			);
-		},
-		start: (...data) => {
-			return console.log(
-				formatData({ level: green(bold('start')), data, time }),
-			);
-		},
-		warn: (...data) => {
-			return console.log(formatData({
+		log: (...data) => console.log(formatData({ data })),
+		info: (...data) =>
+			console.log(formatData({ level: cyan(bold('i')), data })),
+		success: (...data) =>
+			console.log(formatData({ level: green(bold('✔')), data })),
+		start: (...data) =>
+			console.log(formatData({ level: green(bold('start')), data })),
+		warn: (...data) =>
+			console.log(formatData({
 				spaceOut: true,
 				level: bgYellow(bold(' WARN ')),
 				data,
-				time,
-			}));
-		},
-		error: (...data) => {
-			return console.log(formatData({
+			})),
+		error: (...data) =>
+			console.log(formatData({
 				spaceOut: true,
 				level: bgRed(bold(' ERROR ')),
 				data,
-				time,
-			}));
-		},
+			})),
 	};
 }
 
@@ -57,15 +42,16 @@ function formatData(options: {
 	level?: string;
 	spaceOut?: boolean;
 	data: string[];
-	time: string;
 }): string {
-	const { level = '', spaceOut = false, data, time } = options;
-	const templateRegex = /\[(.*?)\]|\((.*?)\)/g;
+	const { level = '', spaceOut = false, data } = options;
+	const time = dim(format(new Date(), 'hh:mm:ss'));
+	const templateRegex = /\[(.*?)\]|\((.*?)\)|\{(.*?)\}/g;
 	const formattedData = data.map((x) => {
 		// @ts-ignore - Deno typings don't seem to have string.replace overloads
-		return x.replace(templateRegex, (_, p1, p2) => {
+		return x.replace(templateRegex, (_, p1, p2, p3) => {
 			if (p1) return cyan(p1);
 			if (p2) return dim(p2);
+			if (p3) return red(p3);
 		});
 	});
 
