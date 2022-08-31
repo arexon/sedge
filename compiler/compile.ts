4f7b2199d3b056f6a7e1a21658bc37c49b5c1f34
arexon <arexonreal@gmail.com>
2022-08-31 23:25:54 +0200

    feat(compiler): compile scripts

--- compiler/compile.ts
+++ compiler/compile.ts
@@ -1,8 +1,11 @@
-import { resolve } from 'path';
+import { transform } from 'esbuild';
+import { extname, resolve } from 'path';
 import { CacheRecord, invalidateCache } from './cache.ts';
 import { loadModule } from './loaders.ts';
 import { Sedge } from './mod.ts';
-import { getTargetPath } from './path.ts';
+import { getTargetPath, toExtension } from './path.ts';
+
+type CompileResult = Promise<'cacheHit' | 'cacheMiss'>;
 
 interface CompileOptions {
 	sedge: Sedge;
@@ -13,9 +16,10 @@ interface CompileOptions {
 
 export async function compileModule(
 	options: CompileOptions,
-): Promise<string> {
+): CompileResult {
 	const { sedge, path, cache, updateCache } = options;
-	const hash = invalidateCache(resolve(path), sedge.fs);
+	const source = sedge.fs.readTextFileSync(path);
+	const hash = invalidateCache(source);
 	const result = await loadModule(resolve(path), {
 		config: sedge.config,
 		fs: sedge.fs,
@@ -33,14 +37,34 @@ export async function compileModule(
 
 export function compileAsset(
 	options: CompileOptions,
-): Promise<string> {
+): CompileResult {
 	const { sedge, path, cache, updateCache } = options;
-	const hash = invalidateCache(resolve(path), sedge.fs);
+	const source = sedge.fs.readTextFileSync(path);
+	const hash = invalidateCache(source);
 
 	if (hash === cache[resolve(path)]) return Promise.resolve('cacheHit');
 	updateCache(hash);
 
 	sedge.fs.copyFileSync(path, resolve(getTargetPath(path, sedge)));
+
+	return Promise.resolve('cacheMiss');
+}
+
+export async function compileScript(
+	options: CompileOptions,
+): CompileResult {
+	const { sedge, path, cache, updateCache } = options;
+	let source = sedge.fs.readTextFileSync(path);
+	const hash = invalidateCache(source);
+
+	if (hash === cache[resolve(path)]) return Promise.resolve('cacheHit');
+	updateCache(hash);
+
+	if (extname(path) === '.ts') source = (await transform(source)).code;
+	sedge.fs.outputTextFileSync(
+		resolve(toExtension(getTargetPath(path, sedge), '.js')),
+		source,
+	);
 
 	return Promise.resolve('cacheMiss');
 }
