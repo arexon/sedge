9895b064533fdee6ad8ce30361aaf8896e1052cb
arexon <arexonreal@gmail.com>
2022-08-31 16:44:25 +0200

    test(compiler): move fs mocks into individual tests

--- compiler/loaders_test.ts
+++ compiler/loaders_test.ts
@@ -1,19 +1,30 @@
 import { assertEquals, assertObjectMatch } from 'testing/asserts.ts';
-import { testFileSystem } from './fs.ts';
+import { SEDGE_NAMESPACE } from '../shared/mod.ts';
+import { SedgeFileSystem } from './fs.ts';
 import { applyConfig, loadConfig, loadModule } from './loaders.ts';
 
-Deno.test('loadConfig', async () => {
-	const config = await loadConfig(
-		'/config.json',
-		testFileSystem,
-	);
+// @ts-expect-error - this is for testing
+const fs: SedgeFileSystem = {
+	import: () => {
+		return Promise.resolve({
+			default: {
+				type: 'foobar',
+				data: { identifier: `${SEDGE_NAMESPACE}:foo` },
+			},
+		});
+	},
+	readJsonFileSync: () => ({ namespace: 'foo' }),
+};
 
-	assertObjectMatch(config, { identifier: 'foo' });
+Deno.test('loadConfig', () => {
+	const config = loadConfig('config.json', fs);
+
+	assertObjectMatch(config, { namespace: 'foo' });
 });
 
 Deno.test('loadModule', async () => {
 	const result = await loadModule('/test.ts', {
-		fs: testFileSystem,
+		fs,
 		config: { namespace: 'test' },
 		cache: {},
 		hash: '',
