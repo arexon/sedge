6b3d119db405a6b32ac0fe48e050ac9146808e2b
arexon <arexonreal@gmail.com>
2022-09-01 12:52:49 +0200

    refactor(compiler): concise cache resolver

--- compiler/compile.ts
+++ compiler/compile.ts
@@ -5,77 +5,73 @@ import { loadModule } from './loaders.ts';
 import { Sedge } from './mod.ts';
 import { getTargetPath, toExtension } from './path.ts';
 
-type CompileResult = Promise<'cacheHit' | 'cacheMiss'>;
+export const compileModule = createResolver(
+	async ({ path, sedge, hash }) => {
+		const result = await loadModule(resolve(path), {
+			config: sedge.config,
+			fs: sedge.fs,
+			hash,
+		});
 
+		sedge.fs.outputModule(
+			resolve(getTargetPath(path, sedge)),
+			result,
+			sedge.config.sedge.minify,
+		);
+	},
+);
+
+export const compileAsset = createResolver(
+	({ path, sedge, source }) => {
+		sedge.fs.outputAsset(
+			resolve(getTargetPath(path, sedge)),
+			source,
+			sedge.config.sedge.minify,
+		);
+	},
+);
+
+export const compileScript = createResolver(
+	async ({ path, sedge, source }) => {
+		const loader = extname(path).substring(1) as Loader;
+
+		sedge.fs.outputTextFileSync(
+			resolve(toExtension(getTargetPath(path, sedge), '.js')),
+			(await transform(source, {
+				loader,
+				minify: sedge.config.sedge.minify,
+			})).code,
+		);
+	},
+);
+
+type CompileResult = Promise<'cacheHit' | 'cacheMiss'>;
 interface CompileOptions {
 	sedge: Sedge;
 	path: string;
 	cache: CacheRecord;
 	updateCache(hash: string): void;
 }
-
-export async function compileModule(
-	options: CompileOptions,
-): CompileResult {
-	const { sedge, path, cache, updateCache } = options;
-	const source = sedge.fs.readTextFileSync(path);
-	const hash = invalidateCache(source, sedge.config);
-
-	const result = await loadModule(resolve(path), {
-		config: sedge.config,
-		fs: sedge.fs,
-		hash,
-	});
-
-	if (hash === cache[resolve(path)]) return Promise.resolve('cacheHit');
-	updateCache(hash);
-
-	sedge.fs.outputModule(
-		resolve(getTargetPath(path, sedge)),
-		result,
-		sedge.config.sedge.minify,
-	);
-
-	return Promise.resolve('cacheMiss');
+interface ResolverOptions {
+	sedge: Sedge;
+	path: string;
+	source: string;
+	hash: string;
 }
 
-export function compileAsset(
-	options: CompileOptions,
-): CompileResult {
-	const { sedge, path, cache, updateCache } = options;
-	const source = sedge.fs.readTextFileSync(path);
-	const hash = invalidateCache(source, sedge.config);
+function createResolver(
+	fn: (options: ResolverOptions) => Promise<void> | void,
+): (options: CompileOptions) => CompileResult {
+	return async (options) => {
+		const { sedge, path, cache, updateCache } = options;
+		const source = sedge.fs.readTextFileSync(path);
+		const hash = invalidateCache(source, sedge.config);
 
-	if (hash === cache[resolve(path)]) return Promise.resolve('cacheHit');
-	updateCache(hash);
+		if (hash === cache[resolve(path)]) return Promise.resolve('cacheHit');
+		updateCache(hash);
 
-	sedge.fs.outputAsset(
-		resolve(getTargetPath(path, sedge)),
-		source,
-		sedge.config.sedge.minify,
-	);
+		await Promise.resolve(fn({ sedge, path, source, hash }));
 
-	return Promise.resolve('cacheMiss');
-}
-
-export async function compileScript(
-	options: CompileOptions,
-): CompileResult {
-	const { sedge, path, cache, updateCache } = options;
-	const source = sedge.fs.readTextFileSync(path);
-	const hash = invalidateCache(source, sedge.config);
-
-	if (hash === cache[resolve(path)]) return Promise.resolve('cacheHit');
-	updateCache(hash);
-
-	const loader = extname(path).substring(1) as Loader;
-	sedge.fs.outputTextFileSync(
-		resolve(toExtension(getTargetPath(path, sedge), '.js')),
-		(await transform(source, {
-			loader,
-			minify: sedge.config.sedge.minify,
-		})).code,
-	);
-
-	return Promise.resolve('cacheMiss');
+		return Promise.resolve('cacheMiss');
+	};
 }
